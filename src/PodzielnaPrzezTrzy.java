9cb9613c77c8504d7150d7e4e15863272f37a249
Alicja Swierkowska <alicja.swierkowska@gmail.com>
2017-09-27 12:35:04 +0200

    Poprawione zadanie nr 6

--- src/PodzielnaPrzezTrzy.java
+++ src/PodzielnaPrzezTrzy.java
@@ -1,37 +1,26 @@
 import java.util.Scanner;
 
 public class PodzielnaPrzezTrzy {
+
     public static void main(String[] args) {
         Scanner guess = new Scanner(System.in);
         System.out.println("Podaj liczbę");
-        int numberIns = guess.nextInt();
+        int number = guess.nextInt();
 
-        for (int number = numberIns; ; ) {
-
-            while (number >= 100 && number <= 200) {
-                if (number % 3 != 0) {
-                    System.out.println("Towja liczba nie jest podzielna przez 3");
-                } else {
-                    System.out.println("Twoja liczba jest ok");
-                    break;
-                }
+        while (number < 100 || number > 200 || (number >= 100 && number <= 200 && number % 3 != 0)) {
+            if (number < 100) {
+                System.out.println("Podana liczba jest za mała");
+            } else if (number >= 100 && number <= 200 && number % 3 != 0) {
+                System.out.println("Towja liczba nie jest podzielna przez 3");
+            } else {
+                System.out.println("Towja liczba jest za duża");
             }
 
-            while (number < 100 || number > 200) {
-                if (number > 200) {
-                    System.out.println("Twoja liczba jest za duża");
-                } else {
-                    System.out.println("Twoja liczba jest za mała");
-                }
-
-                System.out.println("Podaj liczbę");
-                guess.nextInt();
-
-            }
-            guess.close();
+            System.out.println("Podaj liczbę");
+            guess.nextInt();
         }
+        System.out.println("Twoja liczba jest ok");
+        guess.close();
     }
 }
-
-
 
